6ea414aca232456ce6ac9b15ebc5a34c78228614
Rubi Rojas <rer1288@rit.edu>
2025-03-17 17:51:47 -0400

    Add Footer to Home

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,61 +1,48 @@
-// Import React for creating components
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Footer.module.css';
 
-// FontAwesome icon package (add this to your dependencies if not installed already)
-// npm install @fortawesome/react-fontawesome @fortawesome/free-solid-svg-icons
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import PropTypes from 'prop-types';
-
-function Footer(){
-    return(
-        <footer className='footer'>
-            <div className="container">
-                <div className='row'>
-                    <div className='column'>
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.container}>
+                <div className={styles.row}>
+                    <div className={styles.column}>
                         <h2>AirBlue</h2>
-                        <ul className='list'>
+                        <ul className={styles.list}>
                             <li>123-456-7890</li>
                             <li>Rochester, New York</li>
                             <li>1 Lomb Memorial Dr</li>
                         </ul>
                     </div>
-                    <div className='column'>
+                    <div className={styles.column}>
                         <h2>Pages</h2>
-                        <ul className='list'>
-                            <li>       
-                                <Link to="/manage-events" className='pageLinks'>
-                                    Events
-                                </Link>
+                        <ul className={styles.list}>
+                            <li>
+                                <Link to="/events" className={styles.pageLinks}>Events</Link>
                             </li>
-                            <li>       
-                                <Link to="/manage-events" className='pageLinks'>
-                                    Attendees
-                                </Link>
+                            <li>
+                                <Link to="/attendees" className={styles.pageLinks}>Attendees</Link>
                             </li>
-                            <li>       
-                                <Link to="/manage-events" className='pageLinks'>
-                                    Event Types
-                                </Link>
+                            <li>
+                                <Link to="/event-types" className={styles.pageLinks}>Event Types</Link>
                             </li>
-                            <li>       
-                                <Link to="/manage-events" className='pageLinks'>
-                                    Flights
-                                </Link>
+                            <li>
+                                <Link to="/flights" className={styles.pageLinks}>Flights</Link>
                             </li>
                         </ul>
                     </div>
                 </div>
                 <hr />
-                <div className='row'>
-                    <p className='smallColumn'>
-                        &copy;{new Date().getFullYear()} AirBlue | All Rights Reserverd
+                <div className={styles.row}>
+                    <p className={styles.smallColumn}>
+                        &copy; {new Date().getFullYear()} AirBlue | All Rights Reserved
                     </p>
                 </div>
             </div>
         </footer>
-    )
+    );
 }
 
 export default Footer;
