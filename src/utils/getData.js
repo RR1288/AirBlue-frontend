dbd2525dc01c7cd0e3138639aedd1545825f26ea
Rubi Rojas <rer1288@rit.edu>
2025-03-18 12:14:46 -0400

    Integration done, needs css, error handling and validation

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,17 +1,22 @@
-async function getData(endpoint, token) {
-    const res = await fetch(endpoint, {
+async function getData(method, endpoint, body=null) {
+    const token = localStorage.getItem("token");
+    const res = await fetch(`${import.meta.env.VITE_API_URL}${endpoint}`, {
+        method,
         headers: {
-            'Authorization': `Bearer ${token}`, // Attach token to request
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`, // Attach token to request
         },
+        body: JSON.stringify(body)
     });
 
-    const result = await res.json();
-    if (!result.success) {
-        throw new Error(`Error in response: ${result.message}`);
-    }
-    
-    return result.data;
-}
+    // const result = await res.json();
+    // if (!result.success) {
+    //     throw new Error(`Error in response: ${result.message}`);
+    // }
 
+    // return result.data;
+    return res;
+}
 
 export default getData;
