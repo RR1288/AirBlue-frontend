a4198367fd666a5d42c4fb702ed41a38993dc54a
ChristianMohan <christianjmohan@gmail.com>
2025-03-19 14:51:45 -0400

    trying to figure out why qr wont center
    WIP

--- src/pages/Enable2FAPage.jsx
+++ src/pages/Enable2FAPage.jsx
@@ -95,13 +95,15 @@ const Enable2FAPage = () => {
       {qrCodeUrl ? (
         <div>
           <h2>Scan the QR code with your authenticator app</h2>
-          <img src={qrCodeUrl} alt="QR Code for 2FA" className={styles.qrCode} />
-          <button onClick={() => setShowPinModal(true)} className={styles.button}>
-            Verify 2FA Setup
-          </button>
+          <div className='centerQR'>
+            <img src={qrCodeUrl} alt="QR Code for 2FA" className={styles.qrCode} />
+            <button onClick={() => setShowPinModal(true)} className={styles.button}>
+              Verify 2FA Setup
+            </button>
+          </div>
         </div>
       ) : (
-        <button onClick={handleEnable2FA} className={styles.button}>
+        <button onClick={handleEnable2FA} className={styles.button2fa}>
           Enable 2FA
         </button>
       )}
